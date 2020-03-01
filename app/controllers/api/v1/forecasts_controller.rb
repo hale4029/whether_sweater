ae4883ce984516800aab2e2fe30e8b07c018afa2
Harrison Levin <harrison483@gmail.com>
2020-03-01 11:24:05 -0700

    serializer update, routes

---
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ForecastsController < ApplicationController
+
+  def index
+  end
+
+end
--- app/serializers/upsplash_serializer.rb
+++ app/serializers/upsplash_serializer.rb
@@ -1,4 +1,4 @@
-class LandingSerializer
+class UpsplashSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id
 end
