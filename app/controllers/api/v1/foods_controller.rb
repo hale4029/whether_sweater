3640e0a09893c3da7f6146f39930510f664d1eb1
Harrison Levin <harrison483@gmail.com>
2020-03-02 10:52:46 -0700

    geolocation poro, abstract food methods

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,13 +1,6 @@
 class Api::V1::FoodsController < ApplicationController
   def index
-    data = Facade::Food.new(params)
-    require "pry"; binding.pry
-    render json: Food.new(data)
+    food = Facade::Food.new(params)
+    render json: FoodSerializer.new(food)
   end
-
-  # private
-  #
-  # def params
-  #   params.permit(:start, :end, :food)
-  # end
 end
--- app/models/facade/food.rb
+++ app/models/facade/food.rb
@@ -10,26 +10,31 @@ class Facade::Food
   end
 
   def travel_data
-    data ||= GeoLocation.travel_information(@start_location, @end_location)
+    data ||= Poro::GeoLocation.travel_information(@start_location, @end_location)
   end
 
   def end_coordinates
-    travel_data
+    travel_data["end_location"]
   end
 
   def arrival_time
-    travel_data
+    Time.now.to_i + duration
+  end
+
+  def duration
+    seconds ||= travel_data["duration"]["value"]
   end
 
   def travel_time
-    travel_data
+    travel_data["duration"]["text"]
   end
 
   def forecast
-    DarkSkyService.expected_forecast(end_coordinates, arrival_time)
+    forecast = DarkSkyService.expected_forecast(end_coordinates, arrival_time)
+    forecast['currently']['summary']
   end
 
   def restaurant
-    YelpService.find_restaurant(end_coordinates, @food, arrival_time)
+    data = YelpService.find_restaurant(end_coordinates, @food, arrival_time)
   end
 end
